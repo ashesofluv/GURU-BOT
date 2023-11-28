9dab61064a08d3cccfc53e92d22d5ae6c859b4e6
ashesofluv <152229901+ashesofluv@users.noreply.github.com>
2023-11-28 02:02:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['254746523203', 'MIRIAD', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
